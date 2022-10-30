5795eeafa2eea1c17eb20ee7f512860874bc34da
Grise <griseldaremedi@gmail.com>
2022-10-30 16:04:20 -0300

    Updates

---
+++ precache-manifest.360c609da5a1013dab53719d6359d793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db2f7fac9c36257dfaa",
+    "url": "/appEditorMeme/static/css/main.fe233351.chunk.css"
+  },
+  {
+    "revision": "4db2f7fac9c36257dfaa",
+    "url": "/appEditorMeme/static/js/main.4db2f7fa.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/appEditorMeme/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "b5706a0945fc6aa093ef",
+    "url": "/appEditorMeme/static/js/2.b5706a09.chunk.js"
+  },
+  {
+    "revision": "a5863ed3527df2a46b7e",
+    "url": "/appEditorMeme/static/js/runtime~main.a5863ed3.js"
+  },
+  {
+    "revision": "6cf9b38c78641fac9657179ef3a59ebf",
+    "url": "/appEditorMeme/index.html"
+  }
+];
